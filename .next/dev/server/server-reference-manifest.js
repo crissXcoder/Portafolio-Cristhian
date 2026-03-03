1bb316f80194d8d5b1ae14b86d1d7ef7d6d4663b
crissXcoder <cristhian.altamirano.montes@est.una.ac.cr>
2026-03-02 21:38:38 -0600

    chore: remove .next folder from git and update .gitignore

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"g541Cpzzee/PktgMcxZ5x6MlMXJSeijLdJcAQFZ0X44=\"\n}"
--- .next/dev/static/chunks/app_page_tsx_cc4035b2._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_7d5e3f9f._.js",
-  "static/chunks/node_modules_5958f6af._.js"
-],
-    source: "dynamic"
-});
